d98bb2901a7c863443353bb10f4f7183eeb9f056
Priyadharshini12345 <68586208+Priyadharshini12345@users.noreply.github.com>
2022-01-12 13:12:57 +0530

    Add files via upload

---
+++ LAB06/CalculateBonus.java
@@ -0,0 +1,8 @@
+import java.util.*;                                                                                                                                                     
+                                                                                                                                                                        
+public interface CalculateBonus{                                                                                                                                        
+                                                                                                                                                                        
+        public static final double bonusRate=0.25;                                                                                                                      
+        public double bonus();                                                                                                                                          
+                                                                                                                                                                        
+}  
---
+++ LAB06/IncomeTax.java
@@ -0,0 +1,9 @@
+import java.util.*;                                                                                                                                                     
+                                                                                                                                                                        
+public interface IncomeTax{                                                                                                                                             
+                                                                                                                                                                        
+        public static final double tax1=0.10;                                                                                                                           
+        public static final double tax2=0.15;                                                                                                                           
+        public double calculateTax();                                                                                                                                   
+                                                                                                                                                                        
+}    
